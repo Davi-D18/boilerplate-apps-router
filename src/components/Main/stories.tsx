51b2fea63b78b92d93744e0749290ab47362b936
Davi-D18 <luiz.araujo@alunomicrolins.com.br>
2024-12-20 19:23:57 -0300

    style(custom): adicionado uma interface para o componente Main
    configurado o storie para esse componente

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,12 +1,23 @@
 import { Meta, StoryObj } from '@storybook/react'
-import Main from './Main'
+import { Main } from './Main'
 
 export default {
   title: 'Main',
   component: Main,
+  args: {
+    title: 'React Avançado',
+    description: 'TypeScript, ReactJS, NextJS e Styled Components'
+  },
   parameters: {
     layout: 'fullscreen'
   }
 } as Meta
 
 export const Default: StoryObj = {}
+
+export const Basic: StoryObj = {
+  args: {
+    title: 'title basic',
+    description: 'description basic'
+  }
+}
