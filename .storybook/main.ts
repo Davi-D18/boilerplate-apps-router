53434a7df721f2fa5c768e584d23f6bc2bef089d
Davi-D18 <luiz.araujo@alunomicrolins.com.br>
2024-12-20 17:31:55 -0300

    deps(custom): adicionado o storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,17 @@
+const config = {
+  staticDirs: ['../public'], // Carregar arquivos estáticos, como imagens, estilos e arquivos de configuração
+  stories: ['../src/components/**/*.stories.@(ts|tsx)'], // Define os arquivos de histórias do Storybook localizados na pasta de componentes
+  addons: ['@storybook/addon-essentials'],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  },
+  webpackFinal: (config) => {
+    config.resolve.modules.push(`${process.cwd()}/src`) // Adiciona a pasta de componentes ao caminho de resolução do Webpack
+    return config
+  }
+}
+export default config
